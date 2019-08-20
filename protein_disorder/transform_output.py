be6c413cb0b3220703b44206fff3ae0fe01845a0
Bob Costas <finkzane@gmail.com>
2019-08-20 12:28:36 -0700

    added peek for MoronParser

--- protein_disorder/transform_output.py
+++ protein_disorder/transform_output.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import argparse
+import re
 
 def main():
     arg_parser = argparse.ArgumentParser( description = 'Transform output friom IUPred and moreRONN into a format that is more easily-parseable.' )
@@ -13,6 +14,45 @@ def main():
 
 
     args = arg_parser.parse_args() 
+
+    parser = get_parser( args.input )
+
+
+def get_parser( input_fname ):
+    parser = MoronParser()
+
+    if moron_p.peek( input_fname ):
+        pass
+    else:
+        parser = IUPredParser()
+
+        if not parser.peek( input_fname ):
+            raise ValueError( "Input file is not in either IUPred or MoreRONN format" )  
+
+    return parser
+
+class MoronParser:
+
+    def parse( self, input_file ):
+        pass
+
+    def peek( self, fname ):
+        first_re = re.compile( '\(Sequence (\d+) of (\d+)\):' )
+
+        with open( fname, 'r' ) as of:
+            for line in fname:
+
+                if len( line ) and re.findall( first_re, line.strip() ):
+                    return True
+            return False
+
+
+class IUPredParser:
+    def parse( self, input_file ):
+        pass
+
+    def peek( self, fname ):
+        pass
 
 if __name__ == '__main__':
     main()
