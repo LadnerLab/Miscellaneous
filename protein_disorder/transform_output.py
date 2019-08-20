97c973f2dd5a0d1633d12ca54e461e34132c321d
Bob Costas <finkzane@gmail.com>
2019-08-20 13:55:54 -0700

    added IUPred transforming

--- protein_disorder/transform_output.py
+++ protein_disorder/transform_output.py
@@ -48,6 +48,16 @@ class MoronRecord:
         self.seq = seq
         self.score = score
 
+class IUPredRecord:
+    def __init__( self, seq_id = "", pos_scores = None ):
+        if not pos_scores:
+            self.pos_scores = list()
+        else:
+            self.pos_scores = pos_scores
+
+        self.seq_id = seq_id
+
+
 class MoronTransformer:
     def transform( self, moron_rec ):
         return self._transform( moron_rec )
@@ -67,7 +77,16 @@ class MoronTransformer:
 
 class IUPredTransformer:
     def transform( self, str_list ):
-        return _transform( str_list )
+        return self._transform( str_list )
+
+    def _transform( self, record ):
+        output = '%s\t' % ( record.seq_id )
+
+        scores = list()
+
+        for aa, score in record.pos_scores:
+            scores.append( score )
+        return output + ','.join( scores )
 
     def get_transform( self ):
         return self.transform
@@ -101,12 +120,27 @@ class MoronParser:
 
                 if len( line ) and re.match( first_re, line.strip() ):
                     return True
+                else:
+                    return False
             return False
 
 
 class IUPredParser:
     def parse( self, input_file ):
-        pass
+        record = None
+        not_comment = lambda x: x and not( x[ 0 ] == '#' )
+        is_identifier = lambda x: x and x[ 0 ] == '>'
+        is_score = lambda x: not_comment( x ) and x.count( '\t' )
+
+        for line in input_file:
+            line = line.strip()
+            if is_identifier( line ):
+                if record:
+                    yield record
+                record = IUPredRecord( seq_id = line[ 1:: ] )
+            elif is_score( line ):
+                split = line.split( '\t' )
+                record.pos_scores.append( tuple( split[ 1:: ] ) )
 
     def peek( self, fname ):
         with open( fname, 'r' ) as of:
