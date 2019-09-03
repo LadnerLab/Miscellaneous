cae245f1200fe0ffac5bebfe3be1ac118a648b08
Bob Costas <finkzane@gmail.com>
2019-09-03 10:07:05 -0700

    added DataRecord class

--- sprot_trembl/find_changes.py
+++ sprot_trembl/find_changes.py
@@ -2,16 +2,18 @@
 import argparse
 
 def main():
-    args = argparse.ArgumentParser( description = 'Find changes in Taxonomic ID from one version of '
+    argp = argparse.ArgumentParser( description = 'Find changes in Taxonomic ID from one version of '
                                     'sprot/tremble data files to another. Optionally '
                                     'only report results whose ids are in the sequences in '
                                     'a fasta'
                                   )
 
-    args.add_argument( '--first', help = "The first data file to check." )
-    args.add_argument( '--second', help = "The second data file to check." )
-    args.add_argument( '--filter', help = "Fasta file of sequences to include. " )
-    args.add_argument( '--output', help = "Name of file to output." )
+    argp.add_argument( '--first', help = "The first data file to check." )
+    argp.add_argument( '--second', help = "The second data file to check." )
+    argp.add_argument( '--filter', help = "Fasta file of sequences to include. " )
+    argp.add_argument( '--output', help = "Name of file to output." )
+
+    args = argp.parse_args()
 
     # parse sequences from the first and second (only need ID and OX)
 
@@ -23,6 +25,20 @@ def main():
     # filter out IDs that are not from the fasta (if included)
 
     # write the output
+
+class DataRecord:
+    def __init__( self, id = '', ox = '' ):
+        self.id = id
+        self.ox = ox
+
+    def __hash__( self ):
+        return hash( self.id )
+
+    def __eq__( self, other ):
+        return self.id == other.id 
+
+    def __ne__( self, other ):
+        return not self.__eq__( other )
 
 if __name__ == '__main__':
     main()
