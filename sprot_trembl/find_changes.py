aa37be0b2536afc235a86cef3af4c5be591c38fd
Bob Costas <finkzane@gmail.com>
2019-09-03 11:36:18 -0700

    added filtering by target sequences

--- sprot_trembl/find_changes.py
+++ sprot_trembl/find_changes.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import argparse
+import protein_oligo_library as oligo
+import re
 
 def main():
     argp = argparse.ArgumentParser( description = 'Find changes in Taxonomic ID from one version of '
@@ -19,17 +21,44 @@ def main():
     first_recs  = parse_records( args.first )
     second_recs = parse_records( args.second )
 
-    # parse sequences from fasta, grab IDs for each
+    # slightly trim search space
+    to_search = first_recs.keys() & second_recs.keys()
 
+    # parse sequences from fasta, grab IDs for each
+    fasta_records = get_records_from_fasta( args.filter )
 
     # find those that have changed (ID remains, OX different)
+    changed_ids = filter( lambda x: first_recs[ x ] != second_recs[ x ],
+                          to_search
+                        )
 
     # filter out IDs that are not from the fasta (if included)
+    if fasta_records:
+        target_ids = set( changed_ids ) & fasta_records.keys() 
+    else:
+        target_ids = set( changed_ids )
 
     # write the output
 
+def get_records_from_fasta( fname ):
+    records = dict()
+    line_re = re.compile( "ID=([\S]+)\s[\S]+\sOXX=\d*,(\d*),\d*,\d*" )
+    if fname:
+        names, seqs = oligo.read_fasta_lists( fname )
+        for name in names:
+            name_search = re.search( line_re, name )
+            if name_search:
+                id = name_search.group( 1 )
+                ox = name_search.group( 2 )
+
+                # records.append( DataRecord( id = id, ox = ox ) )
+                records[ id ] = str( ox )
+            else:
+                print( "No match: ", name )
+    return records
+
 def parse_records( fname ):
-    records = list()
+    records = dict()
 
     new_record = lambda x: x[ 0 ] == 'ID'
     record_id  = lambda x: x[ 0 ] == 'OX'
@@ -38,13 +67,14 @@ def parse_records( fname ):
     with open( fname, 'r' ) as of:
         for line in of:
             spl = line.strip().split()
-
             try:
                 
                 if new_record( spl ):
-                    records.append( DataRecord( id = spl[ 1 ] ) )
+                    # records.append( DataRecord( id = spl[ 1 ] ) )
+                    records[ spl[ 1 ] ] = ''
+                    last_id = spl[ 1 ]
                 elif record_id( spl ):
-                    records[ -1 ].set_ox( get_id( spl[ 1 ] ) )
+                    records[ last_id ] = get_id( spl[ 1 ] )
             except IndexError:
                 print( line )
                 
