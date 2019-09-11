921e8bcf1a58c5c19fd63a0ad969274ff599606f
Bob Costas <finkzane@gmail.com>
2019-09-11 16:03:29 -0700

    added parse_sub_record, improved pep_name_re

--- sprot_trembl/fix_ids_fasta_and_map.py
+++ sprot_trembl/fix_ids_fasta_and_map.py
@@ -8,6 +8,7 @@ def main():
                                                   "change a map and fasta files."
                                   )
 
+    argp.add_argument( '--substitution_record' )
     argp.add_argument( '--fasta', help = "Comma-separated list of fasta files in which to change ids." )
     argp.add_argument( '--map', help = "Name of map file associating "
                                        "long peptide names to short peptide names to change."
@@ -21,6 +22,7 @@ def main():
     args = argp.parse_args()
 
     fastas = args.fasta.split( ',' )
+    sub_record = parse_sub_record( args.substitution_record )
 
     for fasta in fastas:
         fixed_n, fixed_s = fix_fasta_names( fasta, id_map )
@@ -29,6 +31,21 @@ def main():
         prefix += '_' + args.suffix 
 
         oligo.write_fasta_lists( prefix + '.' + suffix, fixed_n, fixed_s )
+
+def parse_sub_record( fname ):
+    with open( fname, 'r' ) as of:
+        out = dict()
+        for lineno, line in enumerate( of ):
+            if lineno:
+                spl = line.strip().split( '\t' )
+
+                if is_peptide_name( spl[ 0 ] ):
+                    # remove location annotation
+
+                    spl[ 0 ] = '_'.join( spl[ 0 ].split( '_' )[ :-2 ] )
+                    spl[ 1 ] = '_'.join( spl[ 1 ].split( '_' )[ :-2 ] )
+                out[ spl[ 0 ] ] = spl[ 1 ]
+        return out
 
 def fix_fasta_names( fname, id_map ):
     name, seqs = oligo.read_fasta_lists( fname )
@@ -53,7 +70,7 @@ def fix_fasta_names( fname, id_map ):
     return fixed_names, seqs
 
 def is_peptide_name( name ):
-    pep_name_re = r'(\S|\s)*OXX=(\S)*_\d+_\d+'
+    pep_name_re = r'(\s|\S)+(_(\d+)_(\d+)){1}'
     return re.search( pep_name_re, name )
 
 if __name__ == '__main__':
