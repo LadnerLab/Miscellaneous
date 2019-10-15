e297af2733478741993d656b8d6901118d788709
Bob Costas <finkzane@gmail.com>
2019-10-15 12:35:39 -0700

    added extra checks, fixed runtime errs

--- Distances/translate_distances.py
+++ Distances/translate_distances.py
@@ -18,35 +18,57 @@ def main():
 
     args = argp.parse_args()
 
-    input_distances = parse_distances( args.input )
+    out_header, input_distances = parse_distances( args.input )
     input_format = nt_or_aa( input_distances )
     input_map = parse_map( args.map, fmt = input_format )
 
     output_distances = create_distances( input_distances,
                                          input_map
                                        )
+    write_output( args.output, output_distances, header = out_header )
+
+def write_output( out_fname, distances, header = "" ):
+    TAB = '\t'
+
+    with open( out_fname, 'w' ) as of:
+        if header:
+            of.write( f'{header}\n' )
+        for dist in distances:
+            of.write( f"{TAB.join( map( str, dist ) ) }\n" )
 
 def create_distances( in_dist, in_map ):
     output = list()
     hamm = lambda x, y: sum( [ a != b for a, b in zip( x , y ) ] )
+    error = False
 
     for distance in in_dist:
         seq, dist, neighbor = distance
 
-        tran_seq = in_map[ seq ]
-        tran_neighbor = in_map[ neighbor ]
+        try:
+            tran_seq = in_map[ seq ]
+        except KeyError:
+            print( f"Seq Not found in in_map: {seq}." )
+            error = True
 
-        dist = hamm( tran_seq, tran_neighbor )
-        new = tuple( tran_seq, str( dist ), tran_neighbor )
+        try:
+            tran_neighbor = in_map[ neighbor ]
+        except KeyError:
+            print( f"Neighbor Not found in in_map: {neighbor}." )
+            error = True
 
-        output.append( new )
+        if not error:
+            dist = hamm( tran_seq, tran_neighbor )
+            new = tuple( [ tran_seq, str( dist ), tran_neighbor ] )
+            output.append( new )
+        error = False
+
     return output
 
 def parse_map( filename, fmt = CountMode.AA ):
     # input is formatted: NT\tAA, so this allows us to select
     # the necessary index
     key_index = fmt == CountMode.AA
-    value_index = not( key_index )
+    val_index = not( key_index )
     output = dict()
     
     with open( filename, 'r' ) as open_f:
@@ -61,21 +83,24 @@ def parse_map( filename, fmt = CountMode.AA ):
 
 def parse_distances( ifname ):
     output = list()
+    header = ""
 
     with open( ifname, 'r' ) as open_f:
-        for lineno, line in enumerate( ifname ):
+        for lineno, line in enumerate( open_f ):
             if lineno: 
                 spl_str = line.strip().split( '\t' )
 
                 output.append( tuple( spl_str ) )
+            else:
+                header = line.strip()
                 
-    return output
+    return header, output
 
 def nt_or_aa( input_tuples, num_check = 10 ):
     aa_count = 0
     nt_count = 0
 
-    aa = lambda x: set( x ) == set( [ 'A', 'T', 'G', 'C' ] )
+    aa = lambda x: not( set( x ) == set( [ 'A', 'T', 'G', 'C' ] ) )
     
     for x in range( num_check ):
         current_tup = input_tuples[ x ]
@@ -85,7 +110,7 @@ def nt_or_aa( input_tuples, num_check = 10 ):
         else:
             nt_count += 1
 
-        if aa( current_tup[ 1 ] ):
+        if aa( current_tup[ 2 ] ):
             aa_count += 1
         else:
             nt_count += 1
