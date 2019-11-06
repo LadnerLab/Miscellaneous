01fd9a51ac9f8f5ca34396e9e9d64badf9e9cfba
Bob Costas <finkzane@gmail.com>
2019-11-06 15:20:26 -0700

    vertical alignment

--- Distances/closest.cpp
+++ Distances/closest.cpp
@@ -80,7 +80,7 @@ int main( int argc, char **argv )
                         && distance->second.first > dist
                       )
                         {
-                            distance->second.first = dist;
+                            distance->second.first  = dist;
                             distance->second.second = my_seq_inner;
                         }
                 }
